f0a80418878e5f950dbf2ca2b0e34d5857e23a9a
André Oliveira <p32blo@gmail.com>
2025-02-19 22:34:01 +0000

    Start Adding some conversions and output in XML

--- src/from-iso20022.rs
+++ src/from-iso20022.rs
@@ -1,3 +1,10 @@
+use address::ISO_20022;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    let filename = args.get(1).expect("Error getting the 1st arg");
+    let content = std::fs::read_to_string(filename).expect("Failed to read file");
+    let nf: ISO_20022 = content.parse().expect("Error reading");
+    dbg!(nf);
 }
--- src/to-iso20022.rs
+++ src/to-iso20022.rs
@@ -1,3 +1,15 @@
+use address::{ISO_20022, NF_Z10_011};
+
+use serde_xml_rs::to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    let filename = args.get(1).expect("Error getting the 1st arg");
+    let content = std::fs::read_to_string(filename).expect("Failed to read file");
+    let nf: NF_Z10_011 = content.parse().expect("Error reading");
+
+    let iso: ISO_20022 = nf.try_into().expect("This might fail");
+    println!("{}", to_string(&iso).expect("Failed to convert"));
+    ()
 }
