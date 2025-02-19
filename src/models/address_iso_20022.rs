c36cf6e1c85606bbca2eec3024f2dd95dc236553
André Oliveira <p32blo@gmail.com>
2025-02-19 22:34:01 +0000

    Add tests

--- src/models/address_iso_20022.rs
+++ src/models/address_iso_20022.rs
@@ -6,7 +6,7 @@ use std::error::Error;
 use std::str::FromStr;
 
 use super::address_nf_z10_01::NF_Z10_011;
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq)]
 // serde rename struct
 pub struct ISO_20022 {
     Dept: String,
@@ -40,5 +40,35 @@ impl FromStr for ISO_20022 {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         serde_xml_rs::from_str(s).map_err(|e| Box::new(e) as Box<dyn Error>)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let content = r#"
+            <ISO_20022>
+                <Dept></Dept>
+                <SubDept></SubDept>,
+                <StrtNm></StrtNm>,
+                <BldgNb></BldgNb>,
+                <Flr></Flr>,
+                <PstBx></PstBx>,
+                <Room></Room>,
+                <PstCd></PstCd>,
+                <TwnNm></TwnNm>,
+                <TwnLctnNm></TwnLctnNm>,
+                <StrctNm></StrctNm>,
+                <CtrySubDvsn></CtrySubDvsn>,
+                <Ctry></Ctry>,
+            </ISO_20022>
+        "#;
+
+        let parse: ISO_20022 = content.parse().expect("Cannot parse XML!");
+        assert_eq!(parse, ISO_20022::default());
     }
 }
---
+++ tests/integration_test.rs
@@ -0,0 +1,51 @@
+use address::{
+    models::address::Address,
+    repositories::{
+        address_repository::AddressRepository, in_memory_repository::InMemoryRepository,
+    },
+};
+
+fn get_test_addresses() -> Vec<Address> {
+    vec![
+        Address {
+            street: "RUE DES FLEURS".to_string(),
+            building_number: Some("25D".to_string()),
+            postal_code: "33500".to_string(),
+            city: "Paris".to_string(),
+            country: "FR".to_string(),
+            subdivision: None,
+        },
+        Address {
+            street: "VILLA BEAU SOLEIL".to_string(),
+            building_number: Some("LE VILLAGE".to_string()),
+            postal_code: "82500".to_string(),
+            city: "AUTERIVE".to_string(),
+            country: "FR".to_string(),
+            subdivision: None,
+        },
+    ]
+}
+
+#[test]
+fn test_repository() {
+    let mut repo = InMemoryRepository::new();
+
+    let addrs = get_test_addresses();
+
+    // Test Save
+    let id = repo.save(addrs[0].clone()).expect("failed to save");
+
+    // Test Get
+    let found = repo.get(id);
+    assert_eq!(Some(&addrs[0]), found.as_ref());
+
+    // Add another element
+    let id = repo.save(addrs[1].clone()).expect("failed to save");
+
+    // Test Delete
+    repo.delete(id).expect("failed to delete");
+
+    // test List
+    let list = repo.list();
+    assert_eq!(vec![addrs[0].clone()], list);
+}
